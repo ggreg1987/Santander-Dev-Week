0b2fda4dcfbbcf0db302280e4cfced1d5cda70a1
gabriel <gabriel.gregorio1987@gmail.com>
2023-10-16 04:57:53 -0300

    Creating an AccountDTO

---
+++ src/main/java/me/dio/domain/rest/dto/AccountDTO.java
@@ -0,0 +1,20 @@
+package me.dio.domain.rest.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@Builder
+public class AccountDTO
+        extends RepresentationModel<AccountDTO> implements Serializable {
+
+    private Long id;
+    private String number;
+    private String agency;
+    private BigDecimal balance;
+    private BigDecimal limit;
+}
