70aebeaf62552e95724a9770577f8e58be1ca8c8
gabriel <gabriel.gregorio1987@gmail.com>
2023-10-16 04:44:40 -0300

    Creating an UserRepository

---
+++ src/main/java/me/dio/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package me.dio.domain.repository;
+
+import me.dio.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface UserRepository extends JpaRepository<User,Long> {
+}
