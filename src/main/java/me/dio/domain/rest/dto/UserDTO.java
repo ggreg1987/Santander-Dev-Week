0a4bf9f254230f1024fa277487cbdd98d441e68e
gabriel <gabriel.gregorio1987@gmail.com>
2023-10-16 14:48:45 -0300

    Add equals and hashcode

--- src/main/java/me/dio/domain/rest/dto/UserDTO.java
+++ src/main/java/me/dio/domain/rest/dto/UserDTO.java
@@ -6,6 +6,7 @@ import org.springframework.hateoas.RepresentationModel;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
 
 @Data
 @Builder
@@ -18,4 +19,18 @@ public class UserDTO
     private CardDTO cardDTO;
     private List<FeaturesDTO> featuresDTO;
     private List<NewsDTO> newsDTO;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        UserDTO userDTO = (UserDTO) o;
+        return Objects.equals(id, userDTO.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id);
+    }
 }
