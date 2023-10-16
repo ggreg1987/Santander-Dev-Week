76d9a32797bc52aa226e3ecff19d27974a0d5908
gabriel <gabriel.gregorio1987@gmail.com>
2023-10-16 04:48:03 -0300

    Fixing an error, changin table name from News class

--- src/main/java/me/dio/domain/entity/News.java
+++ src/main/java/me/dio/domain/entity/News.java
@@ -6,6 +6,6 @@ import lombok.Builder;
 import java.io.Serializable;
 
 @Builder
-@Entity(name = "tb_features")
+@Entity(name = "tb_news")
 public class News extends FeaturesAndNews implements Serializable {
 }
