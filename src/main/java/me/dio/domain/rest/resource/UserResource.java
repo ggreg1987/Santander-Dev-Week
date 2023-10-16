919134245dd297da3560eae78798e1679e626390
gabriel <gabriel.gregorio1987@gmail.com>
2023-10-16 12:42:25 -0300

    Creating a findById method

--- src/main/java/me/dio/domain/rest/resource/UserResource.java
+++ src/main/java/me/dio/domain/rest/resource/UserResource.java
@@ -5,9 +5,11 @@ import lombok.RequiredArgsConstructor;
 import me.dio.domain.entity.User;
 import me.dio.domain.rest.dto.UserDTO;
 import me.dio.domain.rest.service.UserService;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,8 +19,18 @@ public class UserResource {
     private final UserService service;
     private final ObjectMapper mapper;
 
+    @PostMapping(produces = APPLICATION_JSON_VALUE,consumes = APPLICATION_JSON_VALUE)
+    @ResponseStatus(CREATED)
     public UserDTO save(@RequestBody User user) {
         var entity = service.create(user);
+        var dto = mapper.convertValue(entity,UserDTO.class);
+        return dto;
+    }
+
+    @GetMapping(value = "{id}",produces = APPLICATION_JSON_VALUE)
+    @ResponseStatus(OK)
+    public UserDTO findById(@PathVariable Long id) {
+        var entity = service.findById(id);
         var dto = mapper.convertValue(entity,UserDTO.class);
         return dto;
     }
