1a84f3ecee84137aba2ecf188aca77fbf55ad867
gabriel <gabriel.gregorio1987@gmail.com>
2023-10-16 12:37:11 -0300

    Creating a save method

--- src/main/java/me/dio/domain/rest/resource/UserResource.java
+++ src/main/java/me/dio/domain/rest/resource/UserResource.java
@@ -2,7 +2,10 @@ package me.dio.domain.rest.resource;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import me.dio.domain.entity.User;
+import me.dio.domain.rest.dto.UserDTO;
 import me.dio.domain.rest.service.UserService;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +16,10 @@ public class UserResource {
 
     private final UserService service;
     private final ObjectMapper mapper;
+
+    public UserDTO save(@RequestBody User user) {
+        var entity = service.create(user);
+        var dto = mapper.convertValue(entity,UserDTO.class);
+        return dto;
+    }
 }
