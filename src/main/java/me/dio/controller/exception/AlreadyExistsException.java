ec84a7b0a96a33c446aa5e26f551dcef40365b8d
gabriel <gabriel.gregorio1987@gmail.com>
2023-10-16 20:41:58 -0300

    Creating an exception to Account number exists

---
+++ src/main/java/me/dio/controller/exception/AlreadyExistsException.java
@@ -0,0 +1,8 @@
+package me.dio.controller.exception;
+
+public class AlreadyExistsException extends Exception {
+
+    public AlreadyExistsException(String message) {
+        super(message);
+    }
+}
